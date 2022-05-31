771de1641967503f01596f522bf0a5a9d465b2ac
sotskayaea <92892144+sotskayaea@users.noreply.github.com>
2022-06-01 02:22:28 +0300

    goodjob

--- src/index.js
+++ src/index.js
@@ -1,1 +1,275 @@
-import './sass/main.scss';
+import menu from './menu.json';
+import markup from './markup.hbs';
+
+const jsMenu = document.querySelector('.js-menu');
+jsMenu.innerHTML = markup(menu)
+
+// change light/dark theme
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+const toggle = document.querySelector('.theme-switch__toggle');
+const body = document.querySelector('body');
+
+toggle.addEventListener('change', onChangeTheme);
+
+function onChangeTheme(evt){
+ if (body.classList.contains(`${Theme.DARK}`)){
+  localStorage.setItem('theme', `${Theme.LIGHT}`);
+ }else{
+   localStorage.setItem('theme', `${Theme.DARK}`) 
+ }
+ onLocalStorage();
+}
+
+onLocalStorage();
+
+function onLocalStorage(){
+  if(localStorage.getItem('theme') === `${Theme.LIGHT}`){
+    body.classList.add(`${Theme.LIGHT}`);
+    body.classList.remove(`${Theme.DARK}`);
+    return
+  }
+  if(localStorage.getItem('theme') === `${Theme.DARK}`){
+    body.classList.add(`${Theme.DARK}`);
+    body.classList.remove(`${Theme.LIGHT}`);
+    toggle.checked = true
+    return
+  }
+}
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
